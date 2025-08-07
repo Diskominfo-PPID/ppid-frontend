83b3cb44c1558a7d1c795446471ee3c086efb025
kamaltz <kamaltz.dena16@gmail.com>
2025-08-08 01:08:46 +0700

    fix login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,13 @@ import LoginForm from "@/components/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-50">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+    <div className="flex justify-center items-center min-h-screen bg-slate-50">
+      <div className="p-8 space-y-6 w-full max-w-md bg-white rounded-lg shadow-md">
         <div className="text-center">
-          <h1 className="text-2xl font-bold text-gray-800 mb-2">Login Admin PPID</h1>
-          <p className="text-sm text-gray-600">Masuk ke sistem administrasi PPID</p>
+          <h1 className="mb-2 text-2xl font-bold text-gray-800">Login</h1>
+          <p className="text-sm text-gray-600">
+            Masuk ke sistem administrasi PPID
+          </p>
         </div>
         <LoginForm />
       </div>
